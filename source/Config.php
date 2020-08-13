9fc27a3e23a73685323f566fd64ef718a38fce16
adrian7123 <bueno1dad@gmail.com>
2020-08-13 12:33:44 -0300

    initial commit

--- source/Config.php
+++ source/Config.php
@@ -27,9 +27,9 @@ define("DATA_LAYER_CONFIG", [
     "driver" => "mysql",
     "host" => "localhost",
     "port" => "3306",
-    "dbname" => "Ecommerce",
-    "username" => "root",
-    "passwd" => "toor",
+    "dbname" => "",
+    "username" => "",
+    "passwd" => "",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
